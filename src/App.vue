f11bf7ad2445a22f6b19cb0af2c30666dfff02f5
Mykola Poberezhnyi <mpoberezhnyi.externe@bedrockstreaming.com>
2023-05-14 11:21:52 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Video streaming</h1>
+    </div>
+  </header>
+
+  <main>
+    <video class="video" controls>
+      <source
+        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+      />
+    </video>
+  </main>
+</template>
+
+<style>
+.video {
+  width: 720px;
+}
+</style>
